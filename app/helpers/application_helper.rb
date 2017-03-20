0b847914780f2281f5795153ae07ea4680fe6a7a
lmotlawski <lmotlawski@sigma.ug.edu.pl>
2017-03-20 16:17:48 +0100

    Poprawki

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def markdown(text)
+    return '' unless text.present?
+    @options ||= {
+        autolink: true,
+        link_attributes: {rel: 'nofollow', target: "_blank"}
+    }
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, @options)
+    @markdown.render(text).html_safe
+  end
 end
